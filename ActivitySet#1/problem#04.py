5ca931c2b3930344766c8771d505577740ca1362
Nyanty <blackmagician230703@gmail.com>
2022-05-31 15:58:03 +0530

    Initial commit

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,5 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+# hrs = input("Enter hours? ")
+t = (hrs := float(input("Hrs "))) * (rph := float(input("Rph ")))
+print(t if hrs <= 40 else t * 1.5)
