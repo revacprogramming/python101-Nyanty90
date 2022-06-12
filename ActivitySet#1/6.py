5353b7c705aec65c6c1412d56242280c4f16efa8
Nyanty <blackmagician230703@gmail.com>
2022-06-13 01:49:07 +0530

    Initial commit

--- ActivitySet#1/6.py
+++ ActivitySet#1/6.py
@@ -1,16 +1,24 @@
 # Loops & Iterators
 
-largest = None
-smallest = None
-
+l, s = None, None
 while True:
-    num = input("Enter a number? ")
-
-    if num == "done":
+    n = input("Enter num ")
+    if n == "done":
         break
+    try:
+        n = int(n)
+    except:
+        print("Invalid input")
+        continue
+    if l is None:
+        l = n
+    elif n > l:
+        l = n
+    if s is None:
+        s = n
+    elif n < s:
+        s = n
+    print(n)
 
-    # ...
-
-    print(num)
-
-print("Maximum", largest)
+print("Maximum", l)
+print("Minimum", s)
