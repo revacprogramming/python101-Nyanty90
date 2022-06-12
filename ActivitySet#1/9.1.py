ba457336463654b669761757cc8bb684848d37d7
Nyanty <blackmagician230703@gmail.com>
2022-06-13 03:34:47 +0530

    Initial commit

--- ActivitySet#1/9.1.py
+++ ActivitySet#1/9.1.py
@@ -1,3 +1,10 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+with open((fn := "./py-101/dataset/romeo.txt")) as f:
+    l = [wrd for lne in f for wrd in lne.split()]
+    ul = []
+    for uwrd in l:
+        if uwrd not in ul:
+            ul.append(uwrd)  # list comprehension didnt work here for some reason and I didn't want to spend more time on it :(
+    ul.sort()
+    print(ul)
