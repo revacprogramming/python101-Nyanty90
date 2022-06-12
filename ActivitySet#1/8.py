d500a822774660e28bf986d798a0ae65d5b9a962
Nyanty <blackmagician230703@gmail.com>
2022-06-13 02:12:53 +0530

    Initial commit

--- ActivitySet#1/8.py
+++ ActivitySet#1/8.py
@@ -1,3 +1,9 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+with open((filename := "py-101/dataset/mbox-short.txt")) as f:
+    cnt, ttl = 0, 0
+    for l in f:
+        if l.startswith("X-DSPAM-Confidence:"):
+            cnt += 1
+            ttl += float(l.split()[1])
+    print(ttl/cnt)
